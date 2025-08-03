c9ed710c9d851b5c87c2373ab6c80005b196c4d2
rodrigoqgomez <76918651+rodrigoqgomez@users.noreply.github.com>
2025-08-02 18:53:30 -0600

    app.py

--- app.py
+++ app.py
@@ -150,5 +150,7 @@ def check_card():
         return jsonify({'status': 'error', 'message': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
