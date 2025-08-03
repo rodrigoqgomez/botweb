3e09714061275f778c5d8d8d6267434c76aa457b
rodrigoqgomez <76918651+rodrigoqgomez@users.noreply.github.com>
2025-08-02 19:46:35 -0600

    app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def index():
     return "✅ Railway me está cargando bien."
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))  # Usa el puerto que asigna Railway o 5000 por defecto
     app.run(host="0.0.0.0", port=port)
