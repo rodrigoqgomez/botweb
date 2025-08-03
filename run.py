92bbe7ffb0b6cf758d4f71d66d308dfe2ffe6d31
rodrigoqgomez <76918651+rodrigoqgomez@users.noreply.github.com>
2025-08-02 18:34:02 -0600

    run.py

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
+import os
 import asyncio
 from app import app as flask_app
+from hypercorn.asyncio import serve
+from hypercorn.config import Config
 
 async def run_flask():
-    from hypercorn.asyncio import serve
-    from hypercorn.config import Config
-
     config = Config()
-    config.bind = ["0.0.0.0:5000"]
+    port = int(os.environ.get("PORT", 5000))
+    config.bind = [f"0.0.0.0:{port}"]
     await serve(flask_app, config)
 
 if __name__ == "__main__":
