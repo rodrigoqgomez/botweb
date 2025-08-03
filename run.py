76ac2f46a22d7b99853aca3f2c826461f4850ee8
rodrigoqgomez <76918651+rodrigoqgomez@users.noreply.github.com>
2025-08-02 18:28:54 -0600

    run.py

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import asyncio
 from app import app as flask_app
-import bot
 
 async def run_flask():
     from hypercorn.asyncio import serve
@@ -10,10 +9,5 @@ async def run_flask():
     config.bind = ["0.0.0.0:5000"]
     await serve(flask_app, config)
 
-async def main():
-    bot_task = asyncio.create_task(bot.start_bot())
-    flask_task = asyncio.create_task(run_flask())
-    await asyncio.gather(bot_task, flask_task)
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(run_flask())
